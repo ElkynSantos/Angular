5c76eed44e1843954349c3417a437ee5859f85d6
ElkynSantos <elkynmendoza@unitec.edu>
2023-06-01 21:10:07 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product.models';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'myapp';
+  constructor(private http: HttpClient) {}
+  products: Product[] = [];
+
+  changeTitle() {
+    this.title = 'changed';
+  }
+  ngOnInit() {
+    this.http
+      .get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => {
+        this.products = data;
+      });
+  }
 }
